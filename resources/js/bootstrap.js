3f5d8a8eef39aafc4f5e42c29ad0497f1fd405d8
wb-studio-2023 <for.develop.2023@gmail.com>
2023-03-08 13:41:42 +0900

    fly deploy

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,6 @@
-import _ from 'lodash';
-window._ = _;
+// import _ from 'lodash';
+window._ = require('lodash');
+// window._ = _;
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -7,8 +8,10 @@ window._ = _;
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+// import axios from 'axios';
+// window.axios = axios;
+// import axios from 'axios';
+window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
